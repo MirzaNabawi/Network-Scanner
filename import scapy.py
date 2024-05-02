309e5d90626233b23d967278cc108098eea07adc
KaaloChele <nabawizaman20@gmail.com>
2024-05-02 20:45:15 +0600

    Finding IP using Scapy and  ARP along with list

--- import scapy.py
+++ import scapy.py
@@ -1,6 +1,10 @@
 import scapy.all as scapy
 
-def scan(ip): 
-    scapy.arping(ip) 
+
+def scan(ip):
+    arp_request= scapy.ARP(pdst=ip)
+    
+    print(arp_request.summary()) 
+    scapy.ls(scapy.ARP())
 
 scan("192.168.115.1/24")
