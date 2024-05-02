7fecff40ca4e5e65ea265345dfd628d6120eecb1
KaaloChele <nabawizaman20@gmail.com>
2024-05-03 00:10:43 +0600

    Showing IP using ARP and Broadcast together

--- import scapy.py
+++ import scapy.py
@@ -3,8 +3,12 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request= scapy.ARP(pdst=ip)
-    
-    print(arp_request.summary()) 
+    arp_request.show()
+    broadcast=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    arp_request_broadcast=  broadcast/arp_request
+    arp_request_broadcast.show
+  
     
 
-scan("192.168.115.1/24")#use your IP here by typing \\route -n\\
+scan("192.168.115.1/24")#use your IP here by typing \\route -n in terminal\\
