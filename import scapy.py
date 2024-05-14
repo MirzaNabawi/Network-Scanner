5a4d6c85ddb7b799da64181ab523a4494ece1233
KaaloChele <nabawizaman20@gmail.com>
2024-05-14 23:22:50 +0600

    Using lists

--- import scapy.py
+++ import scapy.py
@@ -5,8 +5,8 @@ def scan(ip):
     arp_request= scapy.ARP(pdst=ip)
     broadcast=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast=  broadcast/arp_request
-    answered,unanswered = scapy.srp(arp_request_broadcast,timeout=1)
-    print(answered.summary())
+    answered_list,unanswered = scapy.srp(arp_request_broadcast,timeout=1)
+    print(answered_list.summary())
   
     
 
