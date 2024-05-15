9101a270cef4e5069f47064682fcef02bf8ff0e6
KaaloChele <nabawizaman20@gmail.com>
2024-05-15 22:04:11 +0600

    Analying Packets using For loops

--- import scapy.py
+++ import scapy.py
@@ -5,8 +5,12 @@ def scan(ip):
     arp_request= scapy.ARP(pdst=ip)
     broadcast=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast=  broadcast/arp_request
-    answered_list,unanswered = scapy.srp(arp_request_broadcast,timeout=1)
-    print(answered_list.summary())
+    answered_list= scapy.srp(arp_request_broadcast,timeout=1)[0]
+    
+    for elements in answered_list:
+        print(elements[1].psrc)
+        print(elements[1].hwsrc)
+        print("----------------------------------------------------------------------------")
   
     
 
