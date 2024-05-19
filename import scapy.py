78f80149f2cebd9a0362b45d0dc9d892ff8969ef
KaaloChele <nabawizaman20@gmail.com>
2024-05-19 20:31:55 +0600

    Clean way to find IP & MAC Address

--- import scapy.py
+++ import scapy.py
@@ -5,13 +5,14 @@ def scan(ip):
     arp_request= scapy.ARP(pdst=ip)
     broadcast=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast=  broadcast/arp_request
-    answered_list= scapy.srp(arp_request_broadcast,timeout=1)[0]
+    answered_list= scapy.srp(arp_request_broadcast,timeout=1, verbose=False)[0]
     
+    print("IP\t\t\tMAC ADDRESS\n")
     for elements in answered_list:
-        print(elements[1].psrc)
-        print(elements[1].hwsrc)
-        print("----------------------------------------------------------------------------")
+        print(elements[1].psrc + "\t\t" + elements[1].hwsrc)
+        
+        
   
     
 
-scan("192.168.115.1/24")#use your IP here by typing \\route -n in terminal\\
+scan("192.168.0.1/24")#use your IP here by typing \\route -n in terminal\\
