48f64789dfb3b1c837b73dca52700475dac930ee
KaaloChele <nabawizaman20@gmail.com>
2024-05-19 21:09:20 +0600

    Final Code for Network Scanner

--- import scapy.py
+++ import scapy.py
@@ -1,5 +1,11 @@
 import scapy.all as scapy
+import optparse
 
+def get_arguments():
+    parser= optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
+    (options,arguments) = parser.parse_args()
+    return options
 
 def scan(ip):
     arp_request= scapy.ARP(pdst=ip)
@@ -7,15 +13,16 @@ def scan(ip):
     arp_request_broadcast=  broadcast/arp_request
     answered_list= scapy.srp(arp_request_broadcast,timeout=1, verbose=False)[0]
     
-    print("IP\t\t\tMAC ADDRESS\n----------------------------------------------")
     clients_list=[]
     for elements in answered_list:
         client_dictionary = { "ip": elements[1].psrc, "mac": elements[1].hwsrc }
         clients_list.append(client_dictionary)
-        print(elements[1].psrc + "\t\t" + elements[1].hwsrc)
-    print(clients_list)
+    return clients_list
+def print_result(result_list):
+    print("IP\t\t\tMAC ADDRESS\n----------------------------------------------")
+    for clients in result_list:
+        print (clients ["ip"] + "\t\t" + clients ["mac"])
         
-  
-    
-
-scan("192.168.0.1/24")#use your IP here by typing \\route -n in terminal\\
+options = get_arguments()
+scan_result = scan("options.target")
+print_result(scan_result)
