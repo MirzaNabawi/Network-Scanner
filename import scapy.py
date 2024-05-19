1dfe3ffcfe9b6e1cf64baf8505e3d31a1b1c936c
KaaloChele <nabawizaman20@gmail.com>
2024-05-19 20:55:29 +0600

    Adding Dictionary and removing unnecessary ip and MAC address

--- import scapy.py
+++ import scapy.py
@@ -7,10 +7,13 @@ def scan(ip):
     arp_request_broadcast=  broadcast/arp_request
     answered_list= scapy.srp(arp_request_broadcast,timeout=1, verbose=False)[0]
     
-    print("IP\t\t\tMAC ADDRESS\n")
+    print("IP\t\t\tMAC ADDRESS\n----------------------------------------------")
+    clients_list=[]
     for elements in answered_list:
+        client_dictionary = { "ip": elements[1].psrc, "mac": elements[1].hwsrc }
+        clients_list.append(client_dictionary)
         print(elements[1].psrc + "\t\t" + elements[1].hwsrc)
-        
+    print(clients_list)
         
   
     
